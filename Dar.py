84dd4c896ba2f4aec7234ba6907b258477f43c2c
Mdsabbirsrt <Sabbir347iii@gmail.com>
2024-11-20 11:24:43 +0600

    Create Dar.py

---
+++ Dar.py
@@ -0,0 +1,14 @@
+jh,+#'lvzw.jh>%+jh,+#'lvzw.jh>%+jkyս}c1ս}aս}bս}`qս}e1ս}cս}cqս&gM}&*~5[}A@PVY?}b4Fuo_vr	OMi 7fPєM7})CH4o}}`HEOV߉!>)҉,r z8u}y2VB@t>,^eyg*+~0brJ&{t]<2}ߣ()ekt.)	ݮ&Z$eyg*+~0brJ&{4]u2}ߣ()ektZ)ݣ
+O]4Z)eyg*+~0brJ&{uM4Iڮ/߽2VE0E1)Wyw~3)$(kg^}iw~8hv,5=thzy(m5MtXy1]|ҷjwhhǮ^PןZVkݢg!'{]6̣8hv,Qhv,5;-vz{Q )WywJ3)$(kgjwhhǫjx]jwhhǫjx睺]iw[Vkݢg!'{]o-vzmP_2VE0춺ڝ&r"qڞkݢg!'P)Wy(t.Xxgkݢg!'{]K-vzmkݢg!'{]v&jo7jwhhǫjx~kݢg!'{~7\T'̣8hv,5>ڝ&r"qX
+il3)$(kgjwhhǫjxn6kݢg!'{6m4jwhhǫjx]Iڮ/7̣8hv,5=thz
+ilߢLGy(
+],ڝ&r"qڞ]&j~귭jwhhǀ yrOkvjwb֙ݢg!'Q 0ӏAQUaݢjڝڞn^2VE0K-U1-ld
+ilߢLGy(u,4}~}(kgu,ӛ-&j~귭^R@?^}gq+jwhhǒ3{PR3{SR3{U3{U5HτnEu|P@7AA_jwhv)]}4ݢjڝا]u}PAWڝ&݊{u]u_}}PC3^ȭm0am!z(!^8PKDݢjڝا]u]u^뮺뮅$*ږA0_jk.A?jk.A<ܢgqIg5QAڮgn/曊]R@?^}qw
+PxP@Wڝ&݊{u׾Oڝ&ݭ{=kݢjڝا]{A_jwhv)]u]{ￅt^wqA_O{׽3?uܵx}AA,/ޟPL,A?riǛ$j֧hPCWQ/#m%׉QAڮgn/曊]R@?^}gqƷhjwhhǑ0ӴL_~^DQ1}˫A}}5A_~ۮ4ק+rL"v{H^m׎zEAAE_׎}ED楙A?yfxPO7(y(Fjv5h]PB>ƫz۫1@?2VB@u/x=)]@^Yʊ߽(t.Xxg^Ȩ4
+M42}ߡAPxP@W6M5];@4IןrW,^^j^{2Z+,^lz[2צrW}wo0
+A^~f)$E086OV9)zW)Ԅ,@88؟^r_7]4M"~ǥyvU)
+"~ǥyw# Ԏ8؟^r))5#6'zWk{B8
+Hz[1yrC2ͱzڞw2צ>ij;ZjX'z]4M}tM4HEly!M4D_o_}@\zڝ&r"q-)v("y!j^78kyOD.kjnrOjyޯL
+/zxRt81TDX
+D]uTE984DIBӦ~j%u~9]㞻mmM4M5~9=[g.nh$x?'މ)j%lǱ81uŹ"vZb%)K2]p+^~ڮZ(zw)̬hm'讖zZbZ-vwj)bvZ(Zerz{bj[?*^uqjǝZ j0lyׯǿmwh)ޮ+׭j)*^nmv+nW~jxڵz"ܢ"z`zwj׿jv؜%j)ޭ^f)'u}/z{'n'xiޮجzvk蝺ZjWrXܢ/ܡהIׯǿ-vwja"bG͚t߭]k;fW]s^}ڦ/z߮'v\jaz{bq^}ڦ/jY^%jgqFrZ zkQ[.ڝ^u'ܩyi'*'^y+j_iǛ`.+6aߝ:w׽gq_8s]qͷ~ǫrOZqǩrb3^~V{omxzmo橉y(&h)zhǫz)nWZZ^i׫v磲}z?)zhǩ)~հگ>8\j
+8q+עvϾ>,j^؝jG0y(&z޲'뢸gVjSxvZ^q6ְ+^>z$izwbvy%~\zDǩ*'jǩj"jgj)f
